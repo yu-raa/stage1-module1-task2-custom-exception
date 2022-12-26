d8913deb972782c4ea72f2be6e316d58cf92eb85
Yuliya Raikova <yuradev2001@gmail.com>
2022-12-26 14:32:09 +0300

    .

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,11 +1,23 @@
 package com.epam.mjc;
 
+class InvalidStudentIdException extends IllegalArgumentException {
+  public InvalidStudentIdException(String message) {
+    super(message);
+  }
+
+  public InvalidStudentIdException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
 
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+  public Student find(long studentID) throws InvalidStudentIdException {
+    if (Student.getValueOf(studentID) == null) {
+      throw new InvalidStudentIdException(String.format("Could not find student with ID %d", studentID));
+    }
     return Student.getValueOf(studentID);
   }
 
@@ -13,8 +25,13 @@ public class StudentManager {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+      catch (InvalidStudentIdException e) {
+        System.err.println(e.getMessage());
+      }
     }
 
   }
